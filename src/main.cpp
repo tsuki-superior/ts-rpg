495187f1465b40f4920752c6746110424ee80e48
tsuki-superior <tsukisuperior@tutanota.com>
2021-01-22 14:03:32 +0000

    init duktape

--- src/main.cpp
+++ src/main.cpp
@@ -3,3 +3,16 @@
 int main(int argc, char *argv)
 {
 }
+
+MainGame::MainGame()
+{
+    ctx=duk_create_heap_default();
+    if (ctx)
+    {
+        cout << "Duktape Context Created.";
+    }else{
+        cerr << "Duktape Context Failed to create.";
+        exit(-1);
+    }
+
+}
--- src/main.hpp
+++ src/main.hpp
@@ -1,11 +1,15 @@
 #include <SDL2/SDL.h>
 #include <iostream>
+#include <duktape.h>
 
 using namespace std;
 
 class MainGame
 {
 public:
+    MainGame();
     SDL_Renderer *renderer;
     SDL_Window *window;
+protected:
+    duk_context *ctx;
 }
