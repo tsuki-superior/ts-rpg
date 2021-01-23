5a3a45d2b771f5da566ffbc1997e028ae3e7f2fd
Llawliet0872 <arnav0872@gmail.com>
2021-01-23 16:26:23 +0530

    Added comments

--- main.cpp
+++ main.cpp
@@ -3,27 +3,27 @@
 
 int main()
 {
-	Engine engine;
-	Image image;
+	Engine engine; // Declaring the engine
+	Image image; // Declaring an image
 
-	if(engine.init())
+	if(engine.init()) // Initializing the engine
 	{
-		SDL_Texture *sampleTexture = image.loadFromFile(engine.renderer, "red.png");
+		SDL_Texture *sampleTexture = image.loadFromFile(engine.renderer, "red.png"); // Loading an image
 		
-		SDL_Event event;
+		SDL_Event event; // An SDL event for capturing things like closing the window, etc.
 	
 		while(running)
 		{
-			engine.update(event);
-			SDL_RenderClear(engine.renderer);	
-			SDL_RenderCopy(engine.renderer, sampleTexture, NULL, NULL);
-			SDL_RenderPresent(engine.renderer);
+			engine.update(event); // Updating the engine
+			SDL_RenderClear(engine.renderer); // Clearing the buffer of the engine's renderer
+			SDL_RenderCopy(engine.renderer, sampleTexture, NULL, NULL); // Pushing the image to the renderer
+			SDL_RenderPresent(engine.renderer); // Rendering to the screen
 
 		}
 
 	}
 	
-	engine.quit();
+	engine.quit(); // Cleaning the engine up
 	utils::Log("Cleaned up");
 
 	return 0;
